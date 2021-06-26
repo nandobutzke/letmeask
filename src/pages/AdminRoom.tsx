6b7b9d4eeaa6ce1e1ade6bf760c2f2070094cfaa
Fernando Butzke <nandoproject209@gmail.com>
2021-06-26 19:31:16 -0300

    update in Links

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -66,7 +66,7 @@ export function AdminRoom() {
                             isOutlined
                             onClick={() => handleEndRoom()}
                         >
-                            Encerrar aula
+                            Encerrar sala
                         </Button>
                     </div>
                 </div>
